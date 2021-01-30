64408f00b37cdf2714db2a41df42d18604b2dec2
Kathryn A Zurowski <1177-kzurowsk@users.noreply.git.gmu.edu>
2021-01-29 22:58:20 -0500

    Upload existing version of project

---
+++ Basic_IR_script.ps1
@@ -0,0 +1,48 @@
+﻿$destinationDrive = Read-Host("Enter the destination Drive")
+
+#Case information
+$investigator= Read-Host("Enter Investigator Name")
+Write-Output "Collection Script Output Files for CFRS 664 Project 2" | Out-File $destinationDrive\caseInfo.txt 
+Write-Output "Collection Start Time:" | Out-File $destinationDrive\caseInfo.txt -append -nonewline
+Get-Date | Out-File $destinationDrive\caseInfo.txt -append
+Write-Output "Investigator:" $investigator | Out-File $destinationDrive\caseInfo.txt -append
+
+#OS data
+Get-WmiObject win32_operatingsystem | Out-File $destinationDrive\osInfo.txt
+
+#Network Data
+Get-NetNeighbor | Out-File $destinationDrive\arpcache.csv
+Get-NetTCPConnection | Out-File $destinationDrive\ports.txt
+Get-DnsClientCache | Out-File $destinationDrive\DNSCache.txt
+Get-NetRoute | Out-File $destinationDrive\IPRouteTable.txt
+Get-NetAdapter | ConvertTo-html| Out-File $destinationDrive\MACInfo.html
+Get-NetIPAddress | Out-File $destinationDrive\IPInterface.txt
+
+#Services and Processes
+#Get-Service | ConvertTo-html | Out-File $destinationDrive\services.html
+#Get-Process | ConvertTo-Html | Out-File $destinationDrive\processes.html
+
+#Event log data
+Get-EventLog -logname System | ConvertTo-html | Out-File $destinationDrive\sysEvents.html
+Get-EventLog -logname Security | ConvertTo-html | Out-File $destinationDrive\secEvents.html
+Get-EventLog -logname Application | ConvertTo-html | Out-File $destinationDrive\appEvents.html
+
+#File information, includes outputs for entire file system, and specifically prefetch to ease of use
+Get-ChildItem -Path C:\ -Recurse | Select-Object -Property fullname, creationtime, lastaccesstime, lastwrittentime, length |ConvertTo-html | Out-File $destinationDrive\directoryinfo.html
+Get-ChildItem -path C:\Windows\Prefetch | select-object -Property name, lastwritetime | convertTo-html |out-file $destinationDrive\prefetch.html
+
+Get-HotFix | Out-File $destinationDrive\hotfixes.csv
+
+
+
+#Finishing info: end times and file hashes of generated files
+Write-Output "Collection End Time:" | Out-File $destinationDrive\caseInfo.txt -append -nonewline
+Get-Date | Out-File $destinationDrive\caseInfo.txt -append
+
+$path = Get-ChildItem $destinationDrive
+ForEach ($i in $path){
+    Get-FileHash $i.FullName -Algorithm MD5 | Format-List | out-file $destinationDrive\hashes.txt -Append
+}
+
+
+Write-Host "Script complete."
